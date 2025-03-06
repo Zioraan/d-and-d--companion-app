50592a4343bc80396dcc358d210af7ae1e92b4bf
Zioraan <ziondrago1@gmail.com>
2025-03-06 18:23:50 -0500

    Commented out template code

--- src/front/pages/Demo.jsx
+++ src/front/pages/Demo.jsx
@@ -7,37 +7,38 @@ export const Demo = () => {
   const { store, dispatch } = useGlobalReducer()
 
   return (
-    <div className="container">
-      <ul className="list-group">
-        {/* Map over the 'todos' array from the store and render each item as a list element */}
-        {store && store.todos?.map((item) => {
-          return (
-            <li
-              key={item.id}  // React key for list items.
-              className="list-group-item d-flex justify-content-between"
-              style={{ background: item.background }}> 
-              
-              {/* Link to the detail page of this todo. */}
-              <Link to={"/single/" + item.id}>Link to: {item.title} </Link>
-              
-              <p>Open file ./store.js to see the global store that contains and updates the list of colors</p>
-              
-              <button className="btn btn-success" 
-                onClick={() => dispatch({
-                  type: "add_task", 
-                  payload: { id: item.id, color: '#ffa500' }
-                })}>
-                Change Color
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-      <br />
+    // <div className="container">
+    //   <ul className="list-group">
+    //     {/* Map over the 'todos' array from the store and render each item as a list element */}
+    //     {store && store.todos?.map((item) => {
+    //       return (
+    //         <li
+    //           key={item.id}  // React key for list items.
+    //           className="list-group-item d-flex justify-content-between"
+    //           style={{ background: item.background }}> 
 
-      <Link to="/">
-        <button className="btn btn-primary">Back home</button>
-      </Link>
+    //           {/* Link to the detail page of this todo. */}
+    //           <Link to={"/single/" + item.id}>Link to: {item.title} </Link>
+
+    //           <p>Open file ./store.js to see the global store that contains and updates the list of colors</p>
+
+    //           <button className="btn btn-success" 
+    //             onClick={() => dispatch({
+    //               type: "add_task", 
+    //               payload: { id: item.id, color: '#ffa500' }
+    //             })}>
+    //             Change Color
+    //           </button>
+    //         </li>
+    //       );
+    //     })}
+    //   </ul>
+    //   <br />
+
+    //   <Link to="/">
+    //     <button className="btn btn-primary">Back home</button>
+    //   </Link>
+    <div>
     </div>
   );
 };
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -6,30 +6,29 @@ export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
-	const loadMessage = async () => {
-		try {
-			const backendUrl = import.meta.env.VITE_BACKEND_URL
+	// const loadMessage = async () => {
+	// 	try {
+	// 		const backendUrl = import.meta.env.VITE_BACKEND_URL
 
-			if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
+	// 		if (!backendUrl) throw new Error("VITE_BACKEND_URL is not defined in .env file")
 
-			const response = await fetch(backendUrl + "/hello")
-			const data = await response.json()
+	// 		const response = await fetch(backendUrl + "/hello")
+	// 		const data = await response.json()
 
-			if (response.ok) dispatch({ type: "set_hello", payload: data.message })
+	// 		if (response.ok) dispatch({ type: "set_hello", payload: data.message })
 
-			return data
+	// 		return data
 
-		} catch (error) {
-			if (error.message) throw new Error(
-				`Could not fetch the message from the backend.
-				Please check if the backend is running and the backend port is public.`
-			);
-		}
+	// 	} catch (error) {
+	// 		if (error.message) throw new Error(
+	// 			`Could not fetch the message from the backend.
+	// 			Please check if the backend is running and the backend port is public.`
+	// 		);
+	// 	}
 
-	}
+	// }
 
 	useEffect(() => {
-		loadMessage()
 	}, [])
 
 	return (
